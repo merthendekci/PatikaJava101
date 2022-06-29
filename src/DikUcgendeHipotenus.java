467916035b90072b4fb30e9ad75ec531de2fd857
merthendekci <64520974+merthendekci@users.noreply.github.com>
2022-06-29 21:14:09 +0300

    Add files via upload

---
+++ src/DikUcgendeHipotenus.java
@@ -0,0 +1,26 @@
+﻿import java.util.Scanner;
+public class DikUcgendeHipotenus {
+    public static void main(String[] args) {
+        // copy - paste
+        int a,b,c;
+        double alan,cevre,u,alaninkaresi;
+        Scanner inp=new Scanner(System.in);
+
+        System.out.print("Birinci Kenar uzunlugu:");
+        a=inp.nextInt();
+        System.out.print("ikinci Kenar uzunlugu:");
+        b=inp.nextInt();
+        System.out.print("Ucuncu Kenar uzunlugu:");
+        c=inp.nextInt();
+
+        cevre=(a+b+c);
+        u=(cevre/2);
+        alaninkaresi= u*(u-a)*(u-b)*(u-c);
+        alan=Math.sqrt(alaninkaresi);
+        System.out.println("Ucgeninizin cevresi:"+cevre);
+        System.out.println("Ucgeninizin alanı:"+alan);
+
+
+
+    }
+}
